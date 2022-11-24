ec0177c50fa74e574d6f41435493d08c9960d647
joschif <jonas.simon.fleck@gmail.com>
2022-11-24 12:35:57 +0100

    DISVAE prototype

---
+++ scvi/losses/_decomposed_kld.py
@@ -0,0 +1,132 @@
+import torch
+from torch import logsumexp
+from torch.distributions import Normal
+from torch.distributions import kl_divergence as kl
+
+# Probability distribution utils
+def matrix_log_density_gaussian(x, mean, stdev):
+    """Calculates log density of a Gaussian for all combination of batch pairs of
+    `x` and `mean`. I.e. return tensor of shape `(batch_size, batch_size, dim)`.
+
+    Arguments:
+        x: Float value at which to compute the density. Shape: (batch_size, dim).
+        mean: Float value indicating the mean. Shape: (batch_size, dim).
+        logvar: Float value indicating the standard deviation. Shape: (batch_size, dim).
+        batch_size: Integer indicating the batch size.
+    Returns:
+        log_density: Log density of a Gaussian. Shape: (batch_size, batch_size, dim).
+    """
+    batch_size, dim = x.shape
+    x = x.view(batch_size, 1, dim)
+    mu = mu.view(1, batch_size, dim)
+    logvar = logvar.view(1, batch_size, dim)
+    return log_density_gaussian(x, mean, stdev)
+
+
+def log_density_gaussian(x, mean, stdev):
+    """Calculates log density of a Gaussian.
+
+    Arguments:
+        x: Float value at which to compute the density.
+        mean: Float value indicating the mean.
+        stdev: Float value indicating the log variance.
+    Returns:
+        log_density: Log density of a Gaussian.
+    """
+    x = torch.tensor(x, dtype=torch.float32)
+    mean = torch.tensor(mean, dtype=torch.float32)
+    stdev = torch.tensor(stdev, dtype=torch.float32)
+    normal_dist = Normal(mean, stdev)
+    log_density = normal_dist.log_prob(x)
+    return log_density
+
+
+# Implementation adapted from
+# * https://github.com/YannDubs/disentangling-vae
+# * https://github.com/julian-carpenter/beta-TCVAE
+# * https://github.com/rtqichen/beta-tcvae
+class DecomposedKLDivergence:
+    r"""
+    Calculates KL divergence between given and standard gaussian distributions.
+    Implements three ways to calculate the KL divergence:
+        1. Regular KLD as
+            KL(p, q) = H(p, q) - H(p) = -\int p(x)log(q(x))dx - -\int p(x)log(p(x))dx
+                = 0.5 * [log(|s2|/|s1|) - 1 + tr(s1/s2) + (m1-m2)^2/s2]
+                = 0.5 * [-log(|s1|) - 1 + tr(s1) + m1^2] (if m2 = 0, s2 = 1)
+        2. As proposed in [FactorVAE](https://arxiv.org/abs/1802.05983), which adds a
+            total correlation (TC) term which can be weighted individually.
+        3. As proposed in [Beta-TCVAE](https://arxiv.org/abs/1905.09837), which fully
+            decomposes the KLD into TC, MI and dimension-wise KLD terms with
+            individual weights.
+
+    Parameters
+    ----------
+    data_size
+        Number of cells in the training set.
+    """
+
+    def __init__(self, data_size: int = 1000):
+        self.data_size = data_size
+
+    def __call__(self, q, p, z):
+        """
+        Parameters
+        ----------
+        q
+            A distribution.
+        p
+            A distribution.
+        z
+            Sample from the latent distribution.
+        """
+
+        # Calculate KLD between prior and latent distributions
+        kl_divergence = kl(q, p)
+
+        if self.decompose_method is None:
+            return kl_divergence
+
+        log_pz, log_qz, log_qz_prod, log_qz_cond_x = self._get_kld_components(
+            q.loc, q.scale, z
+        )
+        # Calculate the total correlation term
+        # TC[z] = KL[q(z)||\prod_i z_i]
+        total_correlation = torch.mean(log_qz - log_qz_prod)
+        mutual_information = torch.mean(log_qz_cond_x - log_qz)
+        # dw_kl_divergence is KL[q(z)||p(z)] instead of usual KL[q(z|x)||p(z))]
+        dw_kl_divergence = torch.mean(log_qz_prod - log_pz)
+
+        return dict(
+            kld=kl_divergence,
+            tc=total_correlation,
+            mi=mutual_information,
+            dw_kld=dw_kl_divergence,
+        )
+
+    def _get_kld_components(self, loc, scale, z):
+        batch_size = z.shape[0]
+        norm_const = torch.log(batch_size * self.data_size)
+
+        # Calculate log p(z)
+        # Zero mean and unit variance -> prior
+        zeros = torch.zeros_like(z)
+        log_pz = torch.sum(log_density_gaussian(z, zeros, 1), axis=1)
+
+        # Calculate log q(z|x)
+        log_qz_cond_x = torch.sum(log_density_gaussian(z, loc, scale), axis=1)
+        log_qz_prob = matrix_log_density_gaussian(z, loc, scale)
+
+        log_qz = (
+            logsumexp(
+                torch.sum(log_qz_prob, axis=2, keepdims=False), axis=1, keepdims=False
+            )
+            - norm_const
+        )
+
+        log_qz_prod = torch.sum(
+            logsumexp(log_qz_prob, axis=1, keepdims=False) - norm_const,
+            axis=1,
+            keepdims=False,
+        )
+
+        return log_pz, log_qz, log_qz_prod, log_qz_cond_x
---
+++ scvi/module/_disvae.py
@@ -0,0 +1,203 @@
+from typing import Any, Callable, Iterable, Optional, Tuple
+
+import numpy as np
+import torch
+from torch.distributions import kl_divergence as kl
+
+from scvi.module import VAE
+
+from scvi import REGISTRY_KEYS
+from scvi._compat import Literal
+from scvi._types import LatentDataType
+from scvi.module.base import LossOutput
+from scvi.losses import DecomposedKLDivergence
+from scvi.autotune._types import Tunable
+
+
+# Disentangling VAE model
+class DISVAE(VAE):
+    """
+    Variational auto-encoder model.
+
+    This is an implementation of the scVI model described in :cite:p:`Lopez18`.
+
+    Parameters
+    ----------
+    n_input
+        Number of input genes
+    n_cells
+        Number of cells
+    n_batch
+        Number of batches, if 0, no batch correction is performed.
+    n_labels
+        Number of labels
+    n_hidden
+        Number of nodes per hidden layer
+    n_latent
+        Dimensionality of the latent space
+    n_layers
+        Number of hidden layers used for encoder and decoder NNs
+    n_continuous_cov
+        Number of continuous covarites
+    n_cats_per_cov
+        Number of categories for each extra categorical covariate
+    dropout_rate
+        Dropout rate for neural networks
+    dispersion
+        One of the following
+
+        * ``'gene'`` - dispersion parameter of NB is constant per gene across cells
+        * ``'gene-batch'`` - dispersion can differ between different batches
+        * ``'gene-label'`` - dispersion can differ between different labels
+        * ``'gene-cell'`` - dispersion can differ for every gene in every cell
+    log_variational
+        Log(data+1) prior to encoding for numerical stability. Not normalization.
+    gene_likelihood
+        One of
+
+        * ``'nb'`` - Negative binomial distribution
+        * ``'zinb'`` - Zero-inflated negative binomial distribution
+        * ``'poisson'`` - Poisson distribution
+    latent_distribution
+        One of
+
+        * ``'normal'`` - Isotropic normal
+        * ``'ln'`` - Logistic normal with normal params N(0, 1)
+    encode_covariates
+        Whether to concatenate covariates to expression in encoder
+    deeply_inject_covariates
+        Whether to concatenate covariates into output of hidden layers in encoder/decoder. This option
+        only applies when `n_layers` > 1. The covariates are concatenated to the input of subsequent hidden layers.
+    use_layer_norm
+        Whether to use layer norm in layers
+    use_size_factor_key
+        Use size_factor AnnDataField defined by the user as scaling factor in mean of conditional distribution.
+        Takes priority over `use_observed_lib_size`.
+    use_observed_lib_size
+        Use observed library size for RNA as scaling factor in mean of conditional distribution
+    library_log_means
+        1 x n_batch array of means of the log library sizes. Parameterizes prior on library size if
+        not using observed library size.
+    library_log_vars
+        1 x n_batch array of variances of the log library sizes. Parameterizes prior on library size if
+        not using observed library size.
+    var_activation
+        Callable used to ensure positivity of the variational distributions' variance.
+        When `None`, defaults to `torch.exp`.
+    latent_data_type
+        None or the type of latent data.
+    """
+
+    def __init__(
+        self,
+        n_input: int,
+        n_cells: int = 10000,
+        n_batch: int = 0,
+        n_labels: int = 0,
+        n_hidden: Tunable[int] = 128,
+        n_latent: int = 10,
+        n_layers: int = 1,
+        n_continuous_cov: int = 0,
+        n_cats_per_cov: Optional[Iterable[int]] = None,
+        dropout_rate: float = 0.1,
+        dispersion: str = "gene",
+        log_variational: bool = True,
+        gene_likelihood: Literal["zinb", "nb", "poisson"] = "zinb",
+        latent_distribution: str = "normal",
+        mi_weight: float = 1.0,
+        tc_weight: float = 1.0,
+        kld_weight: float = 1.0,
+        decompose_method: Literal["factor", "tc"] = "factor",
+        encode_covariates: bool = False,
+        deeply_inject_covariates: bool = True,
+        use_batch_norm: Literal["encoder", "decoder", "none", "both"] = "both",
+        use_layer_norm: Literal["encoder", "decoder", "none", "both"] = "none",
+        use_size_factor_key: bool = False,
+        use_observed_lib_size: bool = True,
+        library_log_means: Optional[np.ndarray] = None,
+        library_log_vars: Optional[np.ndarray] = None,
+        var_activation: Optional[Callable] = None,
+        latent_data_type: Optional[LatentDataType] = None,
+    ):
+        super().__init__(
+            n_input,
+            n_labels,
+            n_batch,
+            n_hidden,
+            n_latent,
+            n_layers,
+            n_continuous_cov,
+            n_cats_per_cov,
+            dropout_rate,
+            dispersion,
+            log_variational,
+            gene_likelihood,
+            latent_distribution,
+            encode_covariates,
+            deeply_inject_covariates,
+            use_batch_norm,
+            use_layer_norm,
+            use_size_factor_key,
+            use_observed_lib_size,
+            library_log_means,
+            library_log_vars,
+            var_activation,
+            latent_data_type,
+        )
+
+        self.mi_weight = mi_weight
+        self.tc_weight = tc_weight
+        self.kld_weight = kld_weight
+        self.decompose_method = decompose_method
+
+        self.dkl = DecomposedKLDivergence(data_size=n_cells)
+
+    # Redefine loss to dientangeling loss
+    def loss(
+        self,
+        tensors,
+        inference_outputs,
+        generative_outputs,
+        kl_weight: float = 1.0,
+    ):
+        """Computes the loss function for the model."""
+        x = tensors[REGISTRY_KEYS.X_KEY]
+        kl_components_z = self.dkl(inference_outputs["qz"], generative_outputs["pz"])
+        if not self.use_observed_lib_size:
+            kl_divergence_l = kl(
+                inference_outputs["ql"],
+                generative_outputs["pl"],
+            ).sum(dim=1)
+        else:
+            kl_divergence_l = torch.tensor(0.0, device=x.device)
+
+        reconst_loss = -generative_outputs["px"].log_prob(x).sum(-1)
+
+        if self.decompose_method == "factor":
+            loss_z = (
+                self.kld_weight * kl_components_z["kld"]
+                + self.tc_weight * kl_components_z["tc"]
+            )
+        else:
+            loss_z = (
+                self.kld_weight * kl_components_z["dw_kld"]
+                + self.tc_weight * kl_components_z["tc"]
+                + self.mi_weight * kl_components_z["mi"]
+            )
+
+        loss_for_warmup = loss_z
+        kl_local_no_warmup = kl_divergence_l
+
+        weighted_kl_local = kl_weight * loss_for_warmup + kl_local_no_warmup
+
+        loss = torch.mean(reconst_loss + weighted_kl_local)
+
+        kl_local = dict(
+            kl_divergence_l=kl_divergence_l, kl_divergence_z=kl_components_z["kld"]
+        )
+        return LossOutput(
+            loss=loss,
+            reconstruction_loss=reconst_loss,
+            kl_local=kl_local,
+            extra_metrics=kl_components_z,
+        )
